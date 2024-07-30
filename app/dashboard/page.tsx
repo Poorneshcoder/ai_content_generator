4449cfc091764ea82fc7eb1adf29343298ebf43b
Poorneshcoder <eswar.chinni354@gmail.com>
2024-07-30 16:42:32 +0530

    initial setup and adding templates

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React, { useState } from 'react'
+import SearchSection from './_components/SearchSection'
+import TemplateListSection from './_components/TemplateListSection'
+
+const Dashboard = () => {
+    const [userSearchInput, setUserSearchInput] = useState<string>();
+  return (
+    <div>
+      {/* search section */}
+      <SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)}/>
+
+      {/* template list section */}
+      <TemplateListSection userSearchInput= {userSearchInput}/>
+    </div>
+  )
+}
+
+export default Dashboard
